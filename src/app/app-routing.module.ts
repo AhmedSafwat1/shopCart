f5245bfcbe1296967661f773c8e72aa07b24a042
safwat <Ahmed.Safwat1235@gmail.com>
2019-04-01 21:09:59 +0200

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { LoginComponent } from './login/login.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+import { AdminOrderComponent } from './admin/admin-order/admin-order.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductComponent},
+  {path: 'orders', component: MyOrdersComponent},
+  {path: 'admin/order', component: AdminOrderComponent},
+  {path: 'admin/product', component: AdminProductComponent},
+  {path: 'shoping-cart', component: ShoppingCartComponent},
+  {path: 'login', component: LoginComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+import { AdminOrderComponent } from './admin/admin-order/admin-order.component';
+import { FormsModule } from '@angular/forms';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProductComponent,
+    MyOrdersComponent,
+    AdminProductComponent,
+    AdminOrderComponent,
+    ShoppingCartComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
